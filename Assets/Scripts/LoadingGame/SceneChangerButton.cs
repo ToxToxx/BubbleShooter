2b7f22151af1ad82bcc9cfda8dab337b1889cbcc
Matvey <sinema3d00@gmail.com>
2024-09-27 10:05:44 +0300

    Add scene changer button
    It fits on easy changing scenes by button clicking

---
+++ Assets/Scripts/LoadingGame/SceneChangerButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChangerButton : MonoBehaviour
+{
+    [SerializeField] private Button _changeSceneButton;
+    [SerializeField] private Loader.Scene _concreteScene;
+    private void LoadGame()
+    {
+        Loader.Load(_concreteScene);
+        Time.timeScale = 1.0f;
+    }
+
+    private void OnEnable()
+    {
+        if (_changeSceneButton != null)
+        {
+            _changeSceneButton.onClick.AddListener(LoadGame);
+        }
+        else
+        {
+            Debug.LogError("No scene is existing");
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_changeSceneButton != null)
+        {
+            _changeSceneButton.onClick.RemoveListener(LoadGame);
+        }
+    }
+}
