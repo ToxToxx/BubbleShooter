12d325750e152cf6449a5561e9a7ff2aabbfa154
Matvey <sinema3d00@gmail.com>
2024-10-05 17:50:02 +0300

    remaking architecture for better performance and working

---
+++ Assets/Scripts/GameLogic/Bubble.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Bubble : MonoBehaviour
+{
+    public Color Color { get; private set; }
+    public char ColorId { get; private set; }
+
+    public void Initialize(Color color, char colorId)
+    {
+        Color = color;
+        ColorId = colorId;
+        SetColor(color); 
+    }
+
+    private void SetColor(Color color)
+    {
+        if (TryGetComponent<SpriteRenderer>(out var spriteRenderer))
+        {
+            spriteRenderer.color = color; 
+        }
+    }
+}
